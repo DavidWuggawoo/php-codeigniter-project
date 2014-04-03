57585f22fb7ff8c4148c92a00a71fd39add59832
Aaron Lote <aaron@theaudience.com>
2014-04-03 19:13:36 +0100

    Initial Commit...made a basic controller by playing with a tutorial

---
+++ application/controllers/product.php
@@ -0,0 +1,67 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+
+class Product extends CI_Controller {
+
+
+    private $postFields = array('name', 'description', 'enabled');
+    private $putFields = array('name', 'description', 'enabled');
+    private $deleteFields = array('id');
+
+    public function index() {
+
+        $query = $this->db->query(' SELECT * FROM Product ');
+        $results = array();
+
+        foreach ($query->result() as $row) {
+            $results['products'][] = (object) $row;
+        }
+
+        $results['enabledOptions'] = array('Enabled' => 1, 'Disabled' => 0);
+
+        $this->load->view('product_index', $results);
+    }
+
+
+    public function create() {
+        echo 'create';
+        exit;
+    }
+
+
+    public function update() {
+
+        // this isnt hugely reliable or secure, but I'm a CI newb
+        $id = (int) end($this->uri->segments);
+
+        // if post is empty then we error
+        if (empty($_POST)) {
+            return $this->index();
+        // post isnt empty, so try and save it
+        } else {
+            // create an empty save fields array
+            $saveFields = array();
+            foreach ($this->postFields as $postField) {
+                if (isset($_POST[$postField])) {
+                    $saveFields[$postField] = $_POST[$postField];
+                }
+            }
+
+            $this->db->where('id', $id);
+            if ($this->db->update('Product', $saveFields)) {
+                return $this->load->view('product_update_success');
+            } else {
+                $this->load->view('product_update_success');
+            }
+            exit;
+        }
+    }
+
+
+    public function delete() {
+        echo 'delete';
+        exit;
+    }
+
+
+}
---
+++ application/views/product_index.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Welcome to CodeIgniter</title>
+
+    <style type="text/css">
+
+        ::selection{ background-color: #E13300; color: white; }
+        ::moz-selection{ background-color: #E13300; color: white; }
+        ::webkit-selection{ background-color: #E13300; color: white; }
+
+        body {
+            background-color: #fff;
+            margin: 40px;
+            font: 13px/20px normal Helvetica, Arial, sans-serif;
+            color: #4F5155;
+        }
+
+        a {
+            color: #003399;
+            background-color: transparent;
+            font-weight: normal;
+        }
+
+        h1 {
+            color: #444;
+            background-color: transparent;
+            border-bottom: 1px solid #D0D0D0;
+            font-size: 19px;
+            font-weight: normal;
+            margin: 0 0 14px 0;
+            padding: 14px 15px 10px 15px;
+        }
+
+        code {
+            font-family: Consolas, Monaco, Courier New, Courier, monospace;
+            font-size: 12px;
+            background-color: #f9f9f9;
+            border: 1px solid #D0D0D0;
+            color: #002166;
+            display: block;
+            margin: 14px 0 14px 0;
+            padding: 12px 10px 12px 10px;
+        }
+
+        #body{
+            margin: 0 15px 0 15px;
+        }
+
+        p.footer{
+            text-align: right;
+            font-size: 11px;
+            border-top: 1px solid #D0D0D0;
+            line-height: 32px;
+            padding: 0 10px 0 10px;
+            margin: 20px 0 0 0;
+        }
+
+        #container{
+            margin: 10px;
+            border: 1px solid #D0D0D0;
+            -webkit-box-shadow: 0 0 8px #D0D0D0;
+        }
+    </style>
+</head>
+<body>
+
+<div id="container">
+    <h1>Welcome to Aarons "Everything Must Go" Store!</h1>
+
+    <div id="body">
+
+        <a href="">
+            Create New Product
+        </a>
+        <table>
+            <thead>
+            <td>Name</td>
+            <td>Description</td>
+            <td>Enabled</td>
+            <td>Created</td>
+            <td>Updated</td>
+            </thead>
+            <tbody>
+            <?php foreach ($products as $product) { ?>
+                <tr>
+                    <td><form action="/index.php/product/update/<?php echo $product->id; ?>" method="post">
+                            <input type="hidden" value="<?php echo $product->id; ?>" />
+                        <input name="name" type="name" value="<?php echo $product->name; ?>" />
+                    </td>
+                    <td>
+                        <textarea id="description" name="description"><?php echo $product->description; ?></textarea>
+                    </td>
+                    <td>
+                        <select name="enabled">
+                            <?php foreach ($enabledOptions as $value => $key) { ?>
+                                <option value="<?php echo (int) $key; ?>" <?php if ($product->enabled == $key) { ?>selected<?php } ?>>
+                                    <?php echo $value; ?>
+                                </option>
+                            <?php } ?>
+                        </select>
+                    </td>
+                    <td>
+                        <?php echo $product->created; ?>
+                    </td>
+                    <td>
+                        <?php echo $product->updated; ?>
+                    </td>
+                    <td>
+                        <button class="btn btn-success">Save Row</button>
+                        </form>
+                    </td>
+                </tr>
+            <?php } ?>
+            </tbody>
+        </table>
+
+        <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
+    </div>
+
+</body>
+</html>
