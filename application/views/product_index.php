7d1ba0e5bc43f0a695e601b8f0941f33bc446092
Aaron Lote <aaron@theaudience.com>
2014-04-03 19:42:51 +0100

    Finished up playing with design

--- application/views/product_index.php
+++ application/views/product_index.php
@@ -1,45 +1,49 @@
+<div id="productListContainer">
+    <div id="productCreateContainer">
         <a href="/index.php/product/create" class="btn btn-success">
             Create New Product
         </a>
-        <table class="table table-striped table-bordered">
-            <thead>
-            <td>Name</td>
-            <td>Description</td>
-            <td>Enabled</td>
-            <td>Created</td>
-            <td>Updated</td>
-            <td></td>
-            </thead>
-            <tbody>
-            <?php foreach ($products as $product) { ?>
-                <tr>
-                    <td><form action="/index.php/product/update/<?php echo $product->id; ?>" method="post">
-                            <input type="hidden" value="<?php echo $product->id; ?>" />
+    </div>
+    <table class="table table-striped table-bordered" id="productList">
+        <thead>
+        <td>Name</td>
+        <td>Description</td>
+        <td>Enabled</td>
+        <td>Created</td>
+        <td>Updated</td>
+        <td></td>
+        </thead>
+        <tbody>
+        <?php foreach ($products as $product) { ?>
+            <tr>
+                <td><form action="/index.php/product/update/<?php echo $product->id; ?>" method="post">
+                        <input type="hidden" value="<?php echo $product->id; ?>" />
                         <input name="name" type="name" value="<?php echo $product->name; ?>" />
-                    </td>
-                    <td>
-                        <textarea id="description" name="description"><?php echo $product->description; ?></textarea>
-                    </td>
-                    <td>
-                        <select name="enabled">
-                            <?php foreach ($enabledOptions as $value => $key) { ?>
-                                <option value="<?php echo (int) $key; ?>" <?php if ($product->enabled == $key) { ?>selected<?php } ?>>
-                                    <?php echo $value; ?>
-                                </option>
-                            <?php } ?>
-                        </select>
-                    </td>
-                    <td>
-                        <?php echo $product->created; ?>
-                    </td>
-                    <td>
-                        <?php echo $product->updated; ?>
-                    </td>
-                    <td>
-                        <button class="btn btn-info">Save Row</button>
-                        </form>
-                    </td>
-                </tr>
-            <?php } ?>
-            </tbody>
-        </table>
+                </td>
+                <td>
+                    <textarea id="description" name="description"><?php echo $product->description; ?></textarea>
+                </td>
+                <td>
+                    <select name="enabled">
+                        <?php foreach ($enabledOptions as $value => $key) { ?>
+                            <option value="<?php echo (int) $key; ?>" <?php if ($product->enabled == $key) { ?>selected<?php } ?>>
+                                <?php echo $value; ?>
+                            </option>
+                        <?php } ?>
+                    </select>
+                </td>
+                <td>
+                    <?php echo $product->created; ?>
+                </td>
+                <td>
+                    <?php echo $product->updated; ?>
+                </td>
+                <td>
+                    <button class="btn btn-info">Save Row</button>
+                    </form>
+                </td>
+            </tr>
+        <?php } ?>
+        </tbody>
+    </table>
+</div>
